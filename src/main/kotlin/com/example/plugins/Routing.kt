c853304a35634708663f276d4cc9d7947b685be6
Shunsuke Osa <hhelibebcnofnenamg@gmail.com>
2023-05-26 00:38:56 +0900

    Static page

--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -1,12 +1,11 @@
 package com.example.plugins
 
 import io.ktor.server.routing.*
-import io.ktor.server.response.*
 import io.ktor.server.http.content.*
 import io.ktor.server.application.*
 
 fun Application.configureRouting() {
-
     routing {
+        staticResources("/static", "files")
     }
 }
