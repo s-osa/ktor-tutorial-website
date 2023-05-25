d154c91fff8a17d97b553562fb644def0fa90f20
Shunsuke Osa <hhelibebcnofnenamg@gmail.com>
2023-05-25 23:43:28 +0900

    Initialize the project

---
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -0,0 +1,12 @@
+package com.example.plugins
+
+import io.ktor.server.routing.*
+import io.ktor.server.response.*
+import io.ktor.server.http.content.*
+import io.ktor.server.application.*
+
+fun Application.configureRouting() {
+
+    routing {
+    }
+}
