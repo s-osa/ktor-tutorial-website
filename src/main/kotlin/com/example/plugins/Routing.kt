fff0f2b48e76da2f57561fbe3bd0fd7e9de70fff
Shunsuke Osa <hhelibebcnofnenamg@gmail.com>
2023-05-27 15:04:35 +0900

    HTML page

--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -3,9 +3,35 @@ package com.example.plugins
 import io.ktor.server.routing.*
 import io.ktor.server.http.content.*
 import io.ktor.server.application.*
+import io.ktor.server.freemarker.*
+import io.ktor.server.response.*
+import com.example.models.*
 
 fun Application.configureRouting() {
     routing {
         staticResources("/static", "files")
+        get("/") {
+            call.respondRedirect("articles")
+        }
+        route("articles") {
+            get {
+                call.respond(FreeMarkerContent("index.ftl", mapOf("articles" to articles)))
+            }
+            get("new") {
+                // Show a page with fields for creating a new article
+            }
+            post {
+                // Save an article
+            }
+            get("{id}") {
+                // Show an article with a specific  id
+            }
+            get("{id}/edit") {
+                // Show a page with fields for editing an article
+            }
+            post("{id}") {
+                // Update or delete an article
+            }
+        }
     }
 }
---
+++ src/main/resources/templates/_layout.ftl
@@ -0,0 +1,18 @@
+<#macro header>
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Kotlin Journal</title>
+    </head>
+    <body style="text-align: center; font-family: sans-serif">
+    <img src="/static/ktor_logo.png" alt="ktor logo">
+    <h1>Kotlin Ktor Journal</h1>
+    <p><i>Powered by Ktor & Freemarker!</i></p>
+    <hr>
+    <#nested>
+    <a href="/">Back to the main page</a>
+    </body>
+    </html>
+</#macro>
+
