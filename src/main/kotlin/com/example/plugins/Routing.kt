fefb4a0f2f7be488edeb3f492869b300cef31c93
Shunsuke Osa <hhelibebcnofnenamg@gmail.com>
2023-05-27 15:12:59 +0900

    New article page

--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -6,6 +6,8 @@ import io.ktor.server.application.*
 import io.ktor.server.freemarker.*
 import io.ktor.server.response.*
 import com.example.models.*
+import io.ktor.server.request.*
+import io.ktor.server.util.*
 
 fun Application.configureRouting() {
     routing {
@@ -18,10 +20,15 @@ fun Application.configureRouting() {
                 call.respond(FreeMarkerContent("index.ftl", mapOf("articles" to articles)))
             }
             get("new") {
-                // Show a page with fields for creating a new article
+                call.respond(FreeMarkerContent("new.ftl", model = null))
             }
             post {
-                // Save an article
+                val formParameters = call.receiveParameters()
+                val title = formParameters.getOrFail("title")
+                val body = formParameters.getOrFail("body")
+                val newEntry = Article.newEntry(title, body)
+                articles.add(newEntry)
+                call.respondRedirect("/articles/${newEntry.id}")
             }
             get("{id}") {
                 // Show an article with a specific  id
